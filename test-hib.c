2651a137c7f248c217628f22161e53211726ceef
Rhys Thomas <rhys97@me.com>
2018-03-04 17:01:00 +0000

    Works now w/o delay? Ah well, cool!

--- test-hib.c
+++ test-hib.c
@@ -22,7 +22,8 @@ void main(void)
 #ifdef USE_LPM5
     if (SYSRSTIV == SYSRSTIV_LPM5WU) {
         // System woke-up from LPM4.5.
-        __delay_cycles(5e5);
+//        __delay_cycles(10);
+        // P1.0 ISR will now trigger.
         __bis_SR_register(GIE);
     } else {
         // System woke-up from "cold start".
@@ -30,15 +31,14 @@ void main(void)
     }
 #endif // USE_LPM5
 
+    // ISR returns and continue with main.
     clock_init();
     Hibernus();
     __bis_SR_register(GIE);
 
     uint8_t n;
     while (1) {
-        for (n = 0; n < 256; n++) {
-            led_set(n);
-            __delay_cycles(24e5);
-        }
+        led_set(n++);
+        __delay_cycles(24e5);
     }
 }
